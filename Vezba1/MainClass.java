393dc8fc95470fbfc343883c624a482583cece0c
marijaT13 <tashevska254@gmail.com>
2022-11-10 18:00:38 +0100

    Avtomobil
    Vezba 1:2 metodi za pechatenje na podatoci za avtomobil.

---
+++ Vezba1/MainClass.java
@@ -0,0 +1,11 @@
+package Vezba1;
+
+public class MainClass {
+	public static void main(String[] args) {
+		Avtomobil av=new Avtomobil("Citroen","Xsara","Trula visna",256789, 1999, 
+				"BT-000-AA");
+				av.pMetod();
+				System.out.println(av.vMetod());
+				
+	}
+}
